6aa48e5bfd207ed013bf62a330b47709da42fc52
RubesWorld <hi@rubesworld.live>
2021-02-02 14:42:24 -0800

    Static Classes Page complete

--- anywhere-fitness-front/src/App.js
+++ anywhere-fitness-front/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Login from './components/Login';
-import PrivateRoute from './components/PrivateRoute';
-import SignUpInstructor from './components/SignUpInstructor'
-import MarketingPage from './marketing-comp/marketingPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import SignUpInstructor from "./components/SignUpInstructor";
+import MarketingPage from "./marketing-comp/marketingPage";
 
+//test
+import MyClassesMain from "./components/client/MyClassesMain";
 
 const App = () => {
   return (
     <>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={MarketingPage}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/register" component={SignUpInstructor} />
-        <PrivateRoute path="/dash"/>
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MyClassesMain} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={SignUpInstructor} />
+          <PrivateRoute path="/dash" />
+        </Switch>
+      </Router>
     </>
   );
-}
+};
 export default App;
