d3e3e93bb9215de0f361cbbf28d31889a323ca5a
RubesWorld <hi@rubesworld.live>
2021-02-02 20:38:12 -0800

    Created Store and did index stuff

--- anywhere-fitness-front/src/index.js
+++ anywhere-fitness-front/src/index.js
@@ -2,9 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+//redux things
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+
+//import middlware
+import thunk from "redux-thunk";
+import logger from "logger";
+
+//import reducer when created
+
+function dummyReducer() {
+  return {
+    title: "Hello world! I'm in the Redux store!",
+  };
+}
+
+const store = createStore(dummyReducer, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
